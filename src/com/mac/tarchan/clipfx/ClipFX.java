b3a0eaee8d9a4aa780ad439e3e652081ecb92a51
tarchan <tarchan@gmail.com>
2014-03-20 16:34:24 +0900

    LTSVのラベルを標準的なものに変更

--- src/com/mac/tarchan/clipfx/ClipFX.java
+++ src/com/mac/tarchan/clipfx/ClipFX.java
@@ -30,7 +30,7 @@ public class ClipFX extends Application {
     static {
 //        System.setProperty("java.util.logging.SimpleFormatter.format", "%1$tT %2$s %4$s: %5$s%6$s%n");
         // LTSVフォーマットに変更
-        System.setProperty("java.util.logging.SimpleFormatter.format", "date:%1$tT\tsource:%2$s\tlevel:%4$s\tmessage:%5$s\tthrown:%6$s%n");
+        System.setProperty("java.util.logging.SimpleFormatter.format", "time:%1$tT\tmethod:%2$s\tlevel:%4$s\tmessage:%5$s\tthrown:%6$s%n");
     }
 
     @Override
